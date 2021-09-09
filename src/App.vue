8414423e1ebb2e9fcffe93e64671d4095f05c697
Joel Okoromi <okmarq@gmail.com>
2021-09-09 15:03:09 +0100

    app page updated

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
+  <div id="nav" class="content">
+    <router-link to="/home1">Home1</router-link> |
+    <router-link to="/about1">About1</router-link>
+    <!-- <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link> |
     <router-link to="/experience">Experience</router-link> |
     <router-link to="/portfolio">Portfolio</router-link> |
-    <router-link to="/whatido">WhatIDo</router-link>
-  </div>
+    <router-link to="/whatido">WhatIDo</router-link> -->
 
-  <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <component :is="Component" />
-    </transition>
-  </router-view>
+    <router-view v-slot="{ Component, route }">
+      <transition
+        :enter-active-class="route.meta.enterClass"
+        :leave-active-class="route.meta.leaveClass"
+      >
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
 </template>
 
 <script></script>
