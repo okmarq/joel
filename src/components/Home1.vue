de96a984a903a6e7b454e7f2da65a523e1d087e1
Joel Okoromi <okmarq@gmail.com>
2021-09-09 17:15:49 +0100

    home1 updated

---
+++ src/components/Home1.vue
@@ -0,0 +1,13 @@
+<template>
+  <transition>
+    <div class="page bg-blue-200">
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, nobis
+        dolorem temporibus porro voluptatum possimus corporis minus quia
+        voluptas consequuntur alias. Deserunt facilis eaque magni aut nam,
+        ducimus saepe nostrum?
+      </p>
+    </div>
+  </transition>
+</template>
