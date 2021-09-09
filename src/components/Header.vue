1e29e64803581ed73b76a1b30a79a06a66d4a7d3
Joel Okoromi <okmarq@gmail.com>
2021-09-09 12:55:43 +0100

    heade component added

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header
+    class="
+      fixed
+      top-0
+      w-screen
+      h-20
+      pri-col
+      flex
+      items-center
+      justify-between
+      px-2
+      sm:px-10
+      md:px-20
+    "
+  >
+    <a class="font-title text-xl sm:text-3xl md:text-5xl" href="#">
+      Lari-Lara
+    </a>
+
+    <div class="font-subtitle block lg:hidden">
+      <a href="#">+234 (0) 810-725-0441</a> / <a href="#">okmarq@gmail.com</a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
