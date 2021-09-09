27fb16e54b530aa550fbf4371d27e7cdbc2912bd
Joel Okoromi <okmarq@gmail.com>
2021-09-10 00:21:48 +0100

    paged rearranged

--- src/components/About1.vue
+++ src/components/About1.vue
@@ -1,12 +1,14 @@
 <template>
   <transition>
-    <div class="page bg-red-200">
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus et
-        veniam sapiente, nulla nam veritatis iure. Sed sint ut aut quia
-        accusantium. Quisquam sunt sed eaque autem ea similique. Quae!
-      </p>
+    <div class="page">
+      <div class="bg-red-200">
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus et
+          veniam sapiente, nulla nam veritatis iure. Sed sint ut aut quia
+          accusantium. Quisquam sunt sed eaque autem ea similique. Quae!
+        </p>
+      </div>
     </div>
   </transition>
 </template>
--- src/components/Home1.vue
+++ src/components/Home1.vue
@@ -1,13 +1,15 @@
 <template>
   <transition>
-    <div class="page bg-blue-200">
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, nobis
-        dolorem temporibus porro voluptatum possimus corporis minus quia
-        voluptas consequuntur alias. Deserunt facilis eaque magni aut nam,
-        ducimus saepe nostrum?
-      </p>
+    <div class="page">
+      <div class="bg-blue-200">
+        <h1>Home</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, nobis
+          dolorem temporibus porro voluptatum possimus corporis minus quia
+          voluptas consequuntur alias. Deserunt facilis eaque magni aut nam,
+          ducimus saepe nostrum?
+        </p>
+      </div>
     </div>
   </transition>
 </template>
