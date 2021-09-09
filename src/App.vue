b5e225d8f3e4c9ab37d7640e3aad50ae5ad033f2
Joel Okoromi <okmarq@gmail.com>
2021-09-09 14:13:15 +0100

    app page updated

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/contact">Contact</router-link> |
+    <router-link to="/experience">Experience</router-link> |
+    <router-link to="/portfolio">Portfolio</router-link> |
+    <router-link to="/whatido">WhatIDo</router-link>
   </div>
-  <router-view />
 
-  <swiper
-    :slides-per-view="3"
-    :space-between="50"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-  </swiper>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <script></script>
+
+<style>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+</style>
