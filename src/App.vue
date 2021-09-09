069d93a8d83a3baf33e89d2bcfada0fff9b6039d
Joel Okoromi <okmarq@gmail.com>
2021-09-09 17:15:22 +0100

    app updated

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="nav" class="content">
+  <div class="content">
     <router-link to="/home1">Home1</router-link> |
     <router-link to="/about1">About1</router-link>
     <!-- <router-link to="/">Home</router-link> |
@@ -7,13 +7,12 @@
     <router-link to="/contact">Contact</router-link> |
     <router-link to="/experience">Experience</router-link> |
     <router-link to="/portfolio">Portfolio</router-link> |
-    <router-link to="/whatido">WhatIDo</router-link> -->
+    <router-link to="/whatido">WhatIDo</router-link> route-->
 
-    <router-view v-slot="{ Component, route }">
-      <transition
-        :enter-active-class="route.meta.enterClass"
-        :leave-active-class="route.meta.leaveClass"
-      >
+    <h1 class="animate__animated animate__bounce">An animated element</h1>
+    
+    <router-view v-slot="{ Component }">
+      <transition name="slide" mode="out-in">
         <component :is="Component" />
       </transition>
     </router-view>
@@ -23,13 +22,50 @@
 <script></script>
 
 <style>
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
+.content {
+  width: 100%;
+  min-height: 100vh;
 }
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease-out;
+.page {
+  position: absolute;
+  top: 30px;
+}
+
+a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  margin-right: 1em;
+}
+
+a:hover,
+a.router-link-active {
+  border-bottom: 2px solid #3498db;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.75s ease-out;
+}
+
+.slide-enter-to {
+  position: absolute;
+  right: 0;
+}
+
+.slide-enter-from {
+  position: absolute;
+  right: -100%;
+}
+
+.slide-leave-to {
+  position: absolute;
+  left: -100%;
+}
+
+.slide-leave-from {
+  position: absolute;
+  left: 0;
 }
 </style>
