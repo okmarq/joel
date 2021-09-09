31a586bd1e017c3481176ef11dfa412c9a7bcee3
Joel Okoromi <okmarq@gmail.com>
2021-09-09 14:13:31 +0100

    index.js file updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+// import About from "../views/About.vue";
+// import Contact from "../views/Contact.vue";
+// import Experience from "../views/Experience.vue";
+// import Portfolio from "../views/Portfolio.vue";
+// import WhatIDo from "../views/WhatIDo.vue";
 
 const routes = [
   {
@@ -15,6 +20,26 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () => import("../views/Contact.vue"),
+  },
+  {
+    path: "/experience",
+    name: "Experience",
+    component: () => import("../views/Experience.vue"),
+  },
+  {
+    path: "/portfolio",
+    name: "Portfolio",
+    component: () => import("../views/Portfolio.vue"),
+  },
+  {
+    path: "/whatido",
+    name: "WhatIDo",
+    component: () => import("../views/WhatIDo.vue"),
   },
 ];
 
