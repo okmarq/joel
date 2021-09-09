48ba3f67b8d9f53457b16c5fe602795b1a975ddd
Joel Okoromi <okmarq@gmail.com>
2021-09-09 12:55:03 +0100

    main view added

---
+++ src/views/JoelOkoromi.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <!-- <About /> -->
+    <!-- <Contact /> -->
+    <!-- <Experience /> -->
+    <!-- <Header /> -->
+    <!-- <Home /> -->
+    <!-- <Portfolio /> -->
+    <!-- <Sidebar /> -->
+    <!-- <WhatIDo /> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import About from "@/components/About.vue";
+// import Contact from "@/components/Contact.vue";
+// import Experience from "@/components/Experience.vue";
+// import Header from "@/components/Header.vue";
+// import Home from "@/components/Home.vue";
+// import Portfolio from "@/components/Portfolio.vue";
+// import Sidebar from "@/components/Sidebar.vue";
+// import WhatIDo from "@/components/WhatIDo.vue";
+
+export default {
+  name: "JoelOkoromi",
+  components: {
+    // About,
+    // Contact,
+    // Experience,
+    // Header,
+    // Home,
+    // Portfolio,
+    // Sidebar,
+    // WhatIDo,
+  },
+};
+</script>
