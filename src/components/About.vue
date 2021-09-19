67087f6b4dbe520afeb0c4eba2a3205859d76515
Joel Okoromi <okmarq@gmail.com>
2021-09-19 05:12:39 +0100

    about page done

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,88 +1,81 @@
 <template>
-  <div class="w-full">
-    <div class="">
-      <main class="h-screen">
-        <div class="pt-20 px-2 sm:pl-32 sm:pr-20">
-          <div class="flex mt-6">
-            <div class="w-5/12 ter-border rounded-l-lg hidden md:block">
-              <img
-                class="block max-w-full h-auto mx-auto rounded-l"
-                src="/assets/images/ET8B4412.JPG"
-              />
-            </div>
-            <div
-              class="
-                w-full
-                pri-col
-                rounded-l-lg
-                md:rounded-l-none
-                rounded-r-lg
-                px-2
-                md:px-8
-                py-6
-                text-justify
-                space-y-2
-                bg-pattern-4
-                mb-4
-                md:mb-0
-              "
-            >
-              <h2 class="font-title sec-text text-xl sm:text-2xl md:text-4xl">
-                Creating meaningful solutions with Tech
-              </h2>
-              <img
-                class="
-                  block
-                  md:hidden
-                  max-w-full
-                  h-auto
-                  w-28
-                  mr-2
-                  rounded-l
-                  float-left
-                  ter-border
-                "
-                src="/assets/images/ET8B4412.JPG"
-              />
+  <main class="w-full pt-20 px-2 sm:pl-32 sm:pr-20">
+    <div class="flex mt-6">
+      <div class="w-5/12 border-4 border-tertiary rounded-l-lg hidden lg:block">
+        <img
+          class="block max-w-full h-auto mx-auto rounded-l"
+          src="../assets/images/tilt-head.jpg"
+        />
+      </div>
+      <div
+        class="
+          bg-pattern-4
+          w-full
+          bg-primary
+          rounded-l-lg
+          lg:rounded-l-none
+          rounded-r-lg
+          px-2
+          md:px-8
+          py-6
+          text-justify
+          space-y-2
+          mb-4
+          md:mb-0
+        "
+      >
+        <h2
+          class="
+            font-serif
+            text-secondary text-lg
+            sm:text-2xl
+            md:text-4xl
+            leading-none
+          "
+        >
+          Creating meaningful solutions with Tech
+        </h2>
+        <img
+          class="
+            block
+            lg:hidden
+            max-w-full
+            h-auto
+            w-20
+            sm:w-28
+            mr-2
+            rounded-l
+            float-left
+            border-tertiary
+          "
+          src="../assets/images/tilt-head.jpg"
+        />
+        <div class="font-serif text-xs lg:text-lg space-y-2 md:space-y-4">
+          <p class="">
+            Joel Okoromi enjoys learning and gathering knowledge that builds on
+            his passion for problem-solving in technology. Just as he joined
+            RootHub, the nickname "Laravel" was given to him by the CEO.
+          </p>
 
-              <p class="text-xs sm:text-sm md:text-base">
-                I love learning and gathering knowledge which adds to my passion
-                to create solutions that are meaningful and solve problems in
-                the tech space.
-              </p>
+          <p class="">
+            He is originally from Edo State, born in Lagos but raised in Uyo,
+            Nigeria. He started his programming career in 2016 and then decided
+            to earn a degree in Computer Science in 2020 at the University of
+            the People in Pasadena, California. This decision would give him the
+            knowledge he wouldn't otherwise gain should he continue as a
+            self-taught programmer.
+          </p>
 
-              <p class="text-xs sm:text-sm md:text-base">
-                Just as I joined the RootHub, I was given a nickname "Laravel"
-                by my CEO, however my name is Joel Julius Okoromi. I'm from Edo
-                state, born in Lagos but grew up in the serenity of uyo,
-                Nigeria.
-              </p>
-
-              <p class="text-xs sm:text-sm md:text-base">
-                I began my programming journey in 2016 then decided to get a
-                degree in computer science so I would gain knowledge I wouldn't
-                otherwise gain should I continue as a self-taught programmer.
-                started in 2020, I attended University of the People, Pasedena,
-                Carlifornia for this very purpose.
-              </p>
-
-              <p class="text-xs sm:text-sm md:text-base">
-                I'm a team player with an eye for simple and elegant designs.
-                Additionally, I understand agile development and time
-                management. I possess good organizational and managerial skills
-                which allows me to produce high-quality work in timely manner.
-              </p>
-
-              <p class="text-xs sm:text-sm md:text-base">
-                I do adhere strongly to recommended software and programming
-                paradigms.
-              </p>
-            </div>
-          </div>
+          <p class="">
+            Joel is a team player who understands agile development and firmly
+            adheres to recommended software and programming paradigms. He has
+            time, organizational and managerial skills to produce high quality
+            work on time.
+          </p>
         </div>
-      </main>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -92,4 +85,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.bg-pattern-4 {
+  background-image: url("../assets/images/patterns/celtic-knot.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+</style>
