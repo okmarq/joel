de14b01f212ea6d7e4ff8229a534502e63fb639b
Joel Okoromi <okmarq@gmail.com>
2021-09-24 02:31:25 +0100

    build updated

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import router from "./router";
 import store from "./store";
 import "./assets/css/font.css";
 import "./assets/css/tailwind.css";
-import "./assets/js/owl.carousel.js";
 
 createApp(App).use(store).use(router).mount("#app");
