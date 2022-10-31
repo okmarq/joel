410dbd39b0fd18fae3a11c35df8fabfd380d5c85
Joel Okoromi <okmarq@gmail.com>
2022-10-31 11:44:24 +0100

    feat: complete works list

--- src/App.tsx
+++ src/App.tsx
@@ -25,17 +25,22 @@ function App() {
       </header>
 
       <main className='mb-10'>
-        <section className='py-4 px-4 sm:px-10 md:px-20 lg:px-48'>
-          <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6'>Blog</h2>
+        <section className='py-4 px-4 sm:px-8 md:px-16 lg:px-32'>
+          <div className='flex justify-center sm:justify-start'>
+            <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6'>Work</h2>
+          </div>
 
-          <div className='border-b-2 pb-4 mt-4 mb-8'>
-            <div className='flex flex-col gap-3 pr-4'>
-              <h3 className='font-medium text-2xl md:text-3xl leading-snug'>UI Interactions of the week</h3>
+          <div className='flex sm:flex-row flex-col gap-4 border-b-2 pb-4 mt-4 mb-8'>
+            <div className='sm:w-5/12 border border-red-500'>
+              <img src={Alumni} alt="project" className='rounded-md object-fit w-full' />
+            </div>
+
+            <div className='flex flex-col gap-3 sm:w-7/12'>
+              <h3 className='font-medium text-2xl md:text-3xl leading-snug'>Designing Dashboards</h3>
 
               <div className='text-sm sm:text-base md:text-lg flex gap-2 sm:gap-4'>
-                <span>12 Feb 2019</span>
-                <span>|</span>
-                <span>Express, Handlebars</span>
+                <span className='bg-dark text-white px-2 md:text-lg rounded-r-full rounded-l-full flex items-center'>2020</span>
+                <span className='text-lg md:text-xl text-light'>Dashboard</span>
               </div>
 
               <p className='tracking-wide'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
